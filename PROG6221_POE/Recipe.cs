6495867ea0a5f5ecfbdf4e30de958c6b7929c7a6
Likwa Moyo <ST10132139@vcconnect.edu.za>
2024-04-16 20:14:47 +0200

    Add project files.

---
+++ PROG6221_POE/Recipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POE
+{
+    // Define the Recipe class to hold all the details of the recipe
+   internal class Recipe
+    {
+        public int NumIngredients { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public int NumSteps { get; set; }
+        public List<Step> Steps { get; set; }
+        public double _scaleFactor = 1.0;
+
+        // Method to scale the recipe by a given factor
+        public void ScaleRecipe(double factor)
+        {
+            _scaleFactor = factor;
+            foreach (Ingredient ingredient in Ingredients)
+            {
+                ingredient.Quantity *= factor;
+            }
+        }
+
+        // Method to reset the quantities to the original values
+        public void ResetQuantities()
+        {
+            _scaleFactor = 1.0;
+            foreach (Ingredient ingredient in Ingredients)
+            {
+                ingredient.Quantity /= _scaleFactor;
+            }
+        }
+    }
+}
